dc69849dafc44928df7f44dcef1718193bae120a
Amaal Al Otaibi <hopes.gmo@gmail.com>
2023-03-19 20:12:22 +0300

    working in post and get

--- Sources/App/Controllers/DroneControler.swift
+++ Sources/App/Controllers/DroneControler.swift
@@ -10,12 +10,50 @@ import Vapor
 
 struct DroneControler: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
-        let drones = routes.grouped("drones")
+        let drones = routes.grouped("drone")
         drones.get(use: index)
-    }
-    
-    //Drone route
-    func index(req: Request) throws -> EventLoopFuture<[FDrone]> {
-        return FDrone.query(on: req.db).all()
+        drones.post(use: create)
+        drones.put(use: update)
+        drones.delete( ":id", use: getById)
+        
+        
+        //Drone route
+        func index(req: Request) async throws -> [FDrone] {
+            try await FDrone.query(on: req.db).all()
+        }
+        
+        func create(req: Request) async throws -> FDrone {
+            let drone = try req.content.decode(FDrone.self)
+            try await drone.save(on: req.db)
+            return drone
+        }
+        
+        
+        func update(req: Request) async throws -> HTTPStatus {
+            return .ok
+        }
+        
+        func delete (req: Request) async throws -> HTTPStatus{
+            
+            guard let drone = try  await FDrone.find(req.parameters.get("id"), on: req.db)
+            else{
+                throw Abort(.notFound)
+            }
+            try await drone.delete(on: req.db)
+            
+            return .ok
+            
+        }
+        
+        func getById(req: Request) async throws -> FDrone{
+            guard let drone = try  await FDrone.find(req.parameters.get("id"), on: req.db)
+                    
+            else{
+                throw Abort(.notFound)
+            }
+            
+            return drone
+            
+        }
     }
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,6 +9,26 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+    
+    app.post("pilot") { req -> EventLoopFuture<DPilot> in
+        // 2
+        let pilot = try req.content.decode(DPilot.self)
+        // 3
+        return pilot.save(on: req.db).map {
+            // 4
+            pilot
+        }
+    }
+    
+    app.post("drone") { req -> EventLoopFuture<FDrone> in
+        // 2
+        let drone = try req.content.decode(FDrone.self)
+        // 3
+        return drone.save(on: req.db).map {
+            // 4
+            drone
+        }
+    }
 
     try app.register(collection: DroneControler())
     try app.register(collection: PilotControler())
