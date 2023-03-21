831b54016a612ad45b4ff61ed7e851e6c6ab547f
Amaal Al Otaibi <hopes.gmo@gmail.com>
2023-03-21 14:58:53 +0300

    fixing the update operation

--- Sources/App/Controllers/DroneControler.swift
+++ Sources/App/Controllers/DroneControler.swift
@@ -13,9 +13,10 @@ struct DroneControler: RouteCollection {
         let drones = routes.grouped("drone")
         drones.get(use: getAll) //working
         drones.post(use: create) //working
-        drones.put(use: update) //working
+        drones.put(":id", use: update) //working
         drones.delete( ":id", use: delete) //working
         drones.get( ":id", use: getById) //working
+        
         
         
         //Drone route
@@ -32,15 +33,26 @@ struct DroneControler: RouteCollection {
         
         func update(req: Request) async throws -> HTTPStatus {
             let newDrone = try req.content.decode(FDrone.self)
-                    guard let drone = try await FDrone.find(newDrone.id, on: req.db) else {
-                        throw Abort(.notFound)
-                    }
-                    drone.droneType = newDrone.droneType
-                    drone.feedback = newDrone.feedback
-                    try await drone.save(on: req.db)
-                    return .ok
+            guard let drone = try  await FDrone.find(req.parameters.get("id"), on: req.db)
+            else{
+                throw Abort(.notFound)
+            }
+            drone.droneType = newDrone.droneType
+            drone.feedback = newDrone.feedback
+            try await drone.save(on: req.db)
+            return .ok
         }
         
+//        func update(req: Request)  throws -> EventLoopFuture<FDrone> {
+//            let newDrone = try req.content.decode(FDrone.self)
+//            return FDrone.find(req.parameters.get("id"), on: req.db).unwrap(or: Abort(.notFound)).flatMap{
+//                drone in drone.droneType = newDrone.droneType
+//                return newDrone.save(on: req.db).map{
+//                    FDrone(id: newDrone.id, droneType: newDrone.droneType, feedback: newDrone.feedback)
+//                }
+//            }
+//        }
+            
         func delete (req: Request) async throws -> HTTPStatus{
             
             guard let drone = try  await FDrone.find(req.parameters.get("id"), on: req.db)
